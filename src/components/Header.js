b9b0fb75efc454484e6754ec7231bd9b73127b96
wiaxx <amanda.wikstrom@hotmail.com>
2022-08-16 13:31:43 +0200

    update header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <HeaderDiv>
-      <Title>Translate</Title>
+      <Title>Banger Translate</Title>
     </HeaderDiv>
   );
 };
