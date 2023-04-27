945c267f8b7819b1f4018efa1d10a51b716c61be
wiaxx <amanda.wikstrom@hotmail.com>
2023-04-27 10:34:29 +0200

    feat: add translation languages

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,55 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const InputHolder = styled.div`
+  margin: 10px auto;
+  background-color: white;
+  border-radius: 8px;
+  padding: 15px;
+  width: 500px;
+  padding-bottom: 10px;
+`;
+
+const InputField = styled.input`
+  height: 30px;
+  padding: 4px;
+  margin: 2px;
+  border-radius: 5px;
+  border: 1px solid #afa0bb;
+  text-align-last: center;
+  cursor: pointer;
+`;
+
+const Selections = styled.select`
+  height: 30px;
+  padding: 4px;
+  margin: 2px;
+  border-radius: 5px;
+  cursor: pointer;
+  border: 1px solid #afa0bb;
+`;
+
+const OKButton = styled.button`
+  height: 30px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: 1px solid #afa0bb;
+  background-color: white;
+  margin: 2px;
+  &:hover,
+  &:focus {
+    background-color: #afa0bb;
+    color: white;
+  }
+  &:active {
+    color: black;
+  }
+`;
 
 const Input = () => {
   // const [inputValue, setInputValue] = useState('/Users/jasonwikstrom/Desktop/testing.xlsx');
-  const [inputValue, setInputValue] = useState("");
-  const [isoValue, setIsoValue] = useState("DA");
+  const [inputValue, setInputValue] = useState('');
+  const [isoValue, setIsoValue] = useState('DA');
   const [statusMessage, setStatusMessage] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -13,33 +58,32 @@ const Input = () => {
     setIsLoading(true);
     setStatusMessage();
 
-    if (inputValue.trim() === "") {
-      alert("Please enter a filepath");
-      setInputValue("");
+    if (inputValue.trim() === '') {
+      alert('Please enter a filepath');
+      setInputValue('');
       setIsLoading(false);
       return;
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/translate", {
-        method: "POST",
+      const response = await fetch('http://localhost:5000/api/translate', {
+        method: 'POST',
         body: JSON.stringify({ path: inputValue, iso: isoValue }),
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
       const message = response;
       setIsLoading(false);
       setStatusMessage(message.status);
     } catch (error) {
-      console.error("Something went wrong: ", error);
+      console.error('Something went wrong: ', error);
     }
   };
 
   return (
-    <>
+    <InputHolder>
       <form onSubmit={getTranslations}>
-        <HeaderText>Choose file and translation language:</HeaderText>
         <InputField
           type="file"
           placeholder="excel-file path"
@@ -52,14 +96,17 @@ const Input = () => {
           onChange={(e) => setIsoValue(e.target.value)}
         >
           <option value="DA">Danska</option>
-          <option value="FI">Finska</option>
-          <option value="PL">Polska</option>
-          <option value="NL">Holländska</option>
           <option value="EN-US">Engelska</option>
+          <option value="FI">Finska</option>
+          <option value="FR">Franska</option>
+          <option value="NL">Holländska</option>
+          <option value="NB">Norska</option>
+          <option value="PL">Polska</option>
           <option value="SV">Svenska</option>
         </Selections>
         <OKButton>OK</OKButton>
       </form>
+
       {isLoading ? <pre>Loading...</pre> : null}
       {statusMessage ? (
         statusMessage >= 200 && statusMessage < 300 ? (
@@ -68,47 +115,8 @@ const Input = () => {
           <pre>Someting went wrong</pre>
         )
       ) : null}
-    </>
+    </InputHolder>
   );
 };
-
-const HeaderText = styled.h1`
-  font-family: Times New Roman;
-  color: #4d2a19;
-`;
-
-const InputField = styled.input`
-  width: 400px;
-  height: 30px;
-  padding: 4px;
-  margin: 2px;
-  border-radius: 5px;
-  border: 1px solid grey;
-  text-align-last: center;
-  cursor: pointer;
-`;
-
-const Selections = styled.select`
-  height: 30px;
-  padding: 4px;
-  margin: 2px;
-  border-radius: 5px;
-`;
-
-const OKButton = styled.button`
-  height: 30px;
-  cursor: pointer;
-  border-radius: 5px;
-  border: 1px solid grey;
-  background-color: white;
-  margin: 2px;
-  &:hover,
-  &:focus {
-    color: palevioletred;
-  }
-  &:active {
-    color: red;
-  }
-`;
 
 export default Input;
